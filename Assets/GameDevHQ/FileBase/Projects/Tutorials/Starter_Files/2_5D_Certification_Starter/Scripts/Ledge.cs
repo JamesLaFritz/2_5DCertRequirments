62879bc203f57dc3713b102d2ea5f6aced2658f1
James LaFritz <ktmarine1999@gmail.com>
2021-06-14 12:32:23 -0400

    Ledge Grab Checker Triggers Ledge Checker.

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Ledge : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField, Tag] private string m_grabberTag = "LedgeGrabber";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void OnTriggerEnter(Collider other)
+   {
+      //Debug.Log($"{other.name} with tag {other.tag} as collided with {name}");
+
+      if (!other.CompareTag(m_grabberTag)) return;
+
+      Debug.Log("Collided with Grab Checker");
+      // When Colliding with the player edge grab checker
+      // Trigger the players Edge Hang Animation.
+      // Freeze the players gravity
+   }
 }
